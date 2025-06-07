53785e1e7d9254121751ccdfded23be652b9feb1
Hance08 <hancechin08@gmail.com>
2025-06-07 23:25:52 +0800

    整合多個上下文管理，新增私有路由以保護儀表板、帳戶、交易和投資頁面，提升應用的安全性和用戶體驗。

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,29 +1,42 @@
 import React from 'react';
 import { Routes, Route } from 'react-router-dom';
 import Navbar from './components/Navbar';
+import PrivateRoute from './components/PrivateRoute';
 import Dashboard from './pages/Dashboard';
 import Accounts from './pages/Accounts';
 import Transactions from './pages/Transactions';
 import Investments from './pages/Investments';
 import Login from './pages/Login';
 import Register from './pages/Register';
+import AuthState from './context/AuthState.jsx';
+import AccountState from './context/account/AccountState.jsx';
+import TransactionState from './context/transaction/TransactionState.jsx';
+import DashboardState from './context/dashboard/DashboardState.jsx';
 import './App.css';
 
 function App() {
   return (
-    <>
-      <Navbar />
-      <div className="container">
-        <Routes>
-          <Route path="/" element={<Dashboard />} />
-          <Route path="/accounts" element={<Accounts />} />
-          <Route path="/transactions" element={<Transactions />} />
-          <Route path="/investments" element={<Investments />} />
-          <Route path="/login" element={<Login />} />
-          <Route path="/register" element={<Register />} />
-        </Routes>
-      </div>
-    </>
+    <AuthState>
+      <AccountState>
+        <TransactionState>
+          <DashboardState>
+            <>
+              <Navbar />
+              <div className="container">
+                <Routes>
+                  <Route path="/" element={<PrivateRoute><Dashboard /></PrivateRoute>} />
+                  <Route path="/accounts" element={<PrivateRoute><Accounts /></PrivateRoute>} />
+                  <Route path="/transactions" element={<PrivateRoute><Transactions /></PrivateRoute>} />
+                  <Route path="/investments" element={<PrivateRoute><Investments /></PrivateRoute>} />
+                  <Route path="/login" element={<Login />} />
+                  <Route path="/register" element={<Register />} />
+                </Routes>
+              </div>
+            </>
+          </DashboardState>
+        </TransactionState>
+      </AccountState>
+    </AuthState>
   );
 }
 
