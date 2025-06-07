9fa4c283e6eb44971cb601dedb3008526634bd49
Hance08 <hancechin08@gmail.com>
2025-06-07 23:24:00 +0800

    新增交易管理功能，包括交易表單和交易項目顯示，整合交易上下文以支持交易的新增、刪除和更新操作，提升用戶體驗。

--- frontend/src/pages/Transactions.jsx
+++ frontend/src/pages/Transactions.jsx
@@ -1,12 +1,40 @@
-import React from 'react';
+import React, { useContext, useEffect } from 'react';
+import TransactionContext from '../context/transaction/transactionContext';
+import TransactionForm from '..ˇˇˇˇˇˇˇˇ/components/transaction/TransactionForm';
+import TransactionItem from '../components/transaction/TransactionItem';
 
 const Transactions = () => {
-  return (
-    <div>
-      <h1>收支紀錄</h1>
-      <p>這裡將會顯示每一筆收支紀錄，並提供新增、修改、刪除的功能。</p>
-    </div>
-  );
+    const transactionContext = useContext(TransactionContext);
+    const { transactions, getTransactions, loading } = transactionContext;
+
+    useEffect(() => {
+        getTransactions();
+        // eslint-disable-next-line
+    }, []);
+
+    const pageStyle = {
+        display: 'grid',
+        gridTemplateColumns: '1fr 2fr',
+        gap: '2rem'
+    };
+
+    return (
+        <div style={pageStyle}>
+            <div>
+                <TransactionForm />
+            </div>
+            <div>
+                <h1>交易紀錄</h1>
+                {transactions.length > 0 && !loading ? (
+                    transactions.map(transaction => (
+                        <TransactionItem key={transaction._id} transaction={transaction} />
+                    ))
+                ) : (
+                    <p>您尚未新增任何交易紀錄。</p>
+                )}
+            </div>
+        </div>
+    );
 };
 
 export default Transactions; 
